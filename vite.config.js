64b3abaf3dba6ea9e536d2fea34dd0b41aa38e2f
jeongwookkim <mybiggold@naver.com>
2025-03-10 18:12:53 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/dashboard/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
 });
